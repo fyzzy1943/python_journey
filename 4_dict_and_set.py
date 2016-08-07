13a00d339006127a25d119dbb8ee8528cce33653
fyzzy1943 <fyzzy1943@gmail.com>
2016-08-07 17:20:50 +0800

    dict and set

---
+++ 4_dict_and_set.py
@@ -0,0 +1,24 @@
+s1 = {1, 2, ('a', 'b'), True, '啊'}
+print(s1)
+
+d1={'einstein':95,'faraday':103}
+print(d1)
+print(d1['faraday'])
+print(d1['einstein'])
+
+d1['edison'] = 97
+print(d1)
+
+d1['maxwell'] = None
+print(d1)
+
+d1[True] = True
+print(d1)
+
+d1[1] = False
+print(d1)
+
+d1[(1,)] = (1,)
+print(d1)
+
+print('啊' in s1)
