ba30e7fed3d93117eea7d9d0087f445543c137ab
fyzzy1943 <fyzzy1943@gmail.com>
2016-08-09 10:19:51 +0800

    function 2 done

--- 5_symbol.py
+++ 5_symbol.py
@@ -41,4 +41,17 @@ x, y = move(50, 30, 60, math.pi/6)
 print(x,y)
 
 def quadratic(a, b, c):
+    delta = math.pow(b, 2)-4*a*c
+    if delta<0:
+        return False
 
+    x1 = (-b+math.sqrt(delta))/(2*a)
+    x2 = (-b-math.sqrt(delta))/(2*a)
+    return x1, x2
+
+print(quadratic(5, 10, 3))
+print(quadratic(4, -4, 1))
+print(quadratic(2, 3, 1))
+print(quadratic(1, 3, -4))
+
+### magnificent split line ###
