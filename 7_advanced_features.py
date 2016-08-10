9131ba9f27cda9b1ef385da5c95ac21ae0518375
fyzzy1943 <fyzzy1943@gmail.com>
2016-08-10 17:11:15 +0800

    generator

--- 7_advanced_features.py
+++ 7_advanced_features.py
@@ -53,6 +53,40 @@ print(L)
 L = ['HelLo', 'woRld', 'I', 'lOVe', 'sAbeR!']
 print([s.lower() for s in L])
 
-L = ['HelLo', 'woRld', 3, 'I', 'lOVe', 7, 21]
+L = ['HelLo', 'woRld', 3, 'I', 'lOVe', 7, 'sABeR!', 21]
 print([s.lower() for s in L if isinstance(s, str)])
 print(*[s.lower() for s in L if isinstance(s, str)])
+
+### generator ###
+G = (s.lower() for s in L if isinstance(s, str))
+print(G)
+for _ in G:
+    print(_, end=' ')
+print()
+
+def fib(n):
+    a, b = 0, 1
+    for i in range(n+1):
+        yield b
+        a, b = b, a+b
+    return 'done.'
+
+G = fib(10)
+for _ in G:
+    print(_, end=' ')
+print()
+
+def triangles():
+    line = [1]
+    while True:
+        yield line
+        line = [1]+[line[i]+line[i-1] for i in range(1, len(line))]+[1]
+
+n = 0
+for t in triangles():
+    print(t)
+    n = n+1
+    if  n==10:
+        break
+
+
