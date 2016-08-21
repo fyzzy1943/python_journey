de438808d09933eb799375b814a39dcd235c3176
fyzzy1943 <fyzzy1943@gmail.com>
2016-08-21 23:16:23 +0800

    enum

---
+++ enum_class.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from enum import Enum, unique
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '->', member, '->', member.value)
+
+print(Month['Jan'])
+print(Month.Jan)
+print(Month.Jan == 1)
+
+@unique
+class Weekday(Enum):
+    Sun = 0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+weekday = Weekday
+print(weekday)
+print(weekday(0).value)
+print(weekday.Sat.value)
