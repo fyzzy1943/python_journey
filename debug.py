1fe37a4a2204a5c79b5b80e05c9c4f97bf46cb95
fyzzy1943 <fyzzy1943@gmail.com>
2016-09-08 15:14:12 +0800

    error handling

---
+++ debug.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# a = 0
+# b = 10/a
+# print(b)
+
+### pydev ###
+
+import logging
+
+logging.debug('debug')
+logging.info('info')
+logging.warning('warning')
+logging.warn('warn')
+logging.error('error')
+logging.critical('critical')
+
+print('%s %s', 'hello', 'world')
